db8f96b07a7d4f6ec2fc77e200a740fade36e7a9
Pohrebniak Andrian <pogra29@gmail.com>
2023-10-04 22:49:22 +0300

    2 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, description, original }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+);
+///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+list.insertAdjacentHTML("beforeend", markup.join(""));
+list.addEventListener("click", onClick);
 
-console.log(galleryItems);
-const a = (0x59).toString(2); 
-const b = a.split('');
-console.log(b);
-let sum = 0; 
-for (let i = 0; i < b.length; i++) {
-    if (b[i] === '1') {
-        sum += 1;
+
+function onClick(evt) {
+  evt.preventDefault();
+  const { target } = evt;
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const imgalt = target.alt; // querySelector('.gallery__image').alt;
+  const curruntItems = galleryItems.find(
+    ({ description }) => description === imgalt
+  );
+  
+  const instance = basicLightbox.create(`
+    <img class="img-photo" src="${curruntItems.original}" alt="${curruntItems.description}" width="1280" height="600" >
+`);
+  
+  instance.show();
+  function onKeydown(evt) {
+    if (evt.key === "Escape" || evt.key === " " || evt.key === "Shift") {
+      instance.close();
     }
+  }
+  if(instance.visible()) {
+    window.addEventListener("keydown", onKeydown);
+  }
 }
-console.log(sum);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, description, original }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+);
+///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+list.insertAdjacentHTML("beforeend", markup.join(""));
+list.addEventListener("click", onClick);
 
-console.log(galleryItems);
+function onClick(evt) {
+  evt.preventDefault();
+  // const { target } = evt;
+  // if (!target.classList.contains("gallery__image")) {
+  //   return;
+  // }
+
+//   const imgalt = target.alt; // querySelector('.gallery__image').alt;
+//   const curruntItems = galleryItems.find(
+//     ({ description }) => description === imgalt
+//   );
+
+//   const instance = basicLightbox.create(`
+//     <img class="img-photo" src="${curruntItems.original}" alt="${curruntItems.description}" width="1280" height="600" >
+// `);
+
+  // instance.show();
+  // function onKeydown(evt) {
+  //   if (evt.which === 27 || evt.key === " " || evt.key === "Shift") {
+  //     instance.close();
+  //   }
+  // }
+  // if (instance.visible()) {
+  //   window.addEventListener("keydown", onKeydown);
+  // }
+}
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
